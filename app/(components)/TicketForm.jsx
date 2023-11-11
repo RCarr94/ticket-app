a26ffb1072a70bfa71d113341fb5ff988ec829ed
Ryan Carr <ryan.g.carr@hotmail.com>
2023-11-11 14:11:04 -0500

    add EDITMODE toggle for update and new form

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -3,8 +3,11 @@
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-export default function TicketForm() {
+export default function TicketForm({ ticket }) {
+
+  const EDITMODE = ticket._id === 'new' ? false : true;
   const router = useRouter();
+
   const handleChange = (evt) => {
     const value = evt.target.value;
     const name = evt.target.name;
@@ -40,18 +43,27 @@ export default function TicketForm() {
     category: 'Hardware Problem',
   };
 
+  if (EDITMODE) {
+    startingTicketData['title'] = ticket.title;
+    startingTicketData['description'] = ticket.description;
+    startingTicketData['priority'] = ticket.priority;
+    startingTicketData['progress'] = ticket.progress;
+    startingTicketData['status'] = ticket.status;
+    startingTicketData['category'] = ticket.category;
+  }
+
   const [formData, setFormData] = useState(startingTicketData);
 
   return (
     <div className="flex justify-center">
       <form className="flex flex-col gap-3 w-1/2" method="post" onSubmit={handleSubmit}>
-        <h3>Create Your Ticket</h3>
+        <h3>{EDITMODE ? 'Update Your Ticket' : 'Create Your Ticket'}</h3>
         <label>Title</label>
         <input id="title" name="title" type="text" onChange={handleChange} required={true} value={formData.title} />
-        
+
         <label>Description</label>
         <textarea id="description" name="description" onChange={handleChange} required={true} value={formData.description} rows="5" />
-        
+
         <label>Category</label>
         <select name="category" value={formData.category} onChange={handleChange}>
           <option value="Hardware Problem">Hardware Problem</option>
@@ -61,15 +73,15 @@ export default function TicketForm() {
 
         <label>Priority</label>
         <div>
-          <input id='priority-1' name='priority' type='radio' onChange={handleChange} value={1} checked={formData.priority == 1} />
+          <input id="priority-1" name="priority" type="radio" onChange={handleChange} value={1} checked={formData.priority == 1} />
           <label>1</label>
-          <input id='priority-2' name='priority' type='radio' onChange={handleChange} value={2} checked={formData.priority == 2} />
+          <input id="priority-2" name="priority" type="radio" onChange={handleChange} value={2} checked={formData.priority == 2} />
           <label>2</label>
-          <input id='priority-3' name='priority' type='radio' onChange={handleChange} value={3} checked={formData.priority == 3} />
+          <input id="priority-3" name="priority" type="radio" onChange={handleChange} value={3} checked={formData.priority == 3} />
           <label>3</label>
-          <input id='priority-4' name='priority' type='radio' onChange={handleChange} value={4} checked={formData.priority == 4} />
+          <input id="priority-4" name="priority" type="radio" onChange={handleChange} value={4} checked={formData.priority == 4} />
           <label>4</label>
-          <input id='priority-5' name='priority' type='radio' onChange={handleChange} value={5} checked={formData.priority == 5} />
+          <input id="priority-5" name="priority" type="radio" onChange={handleChange} value={5} checked={formData.priority == 5} />
           <label>5</label>
         </div>
 
@@ -81,7 +93,7 @@ export default function TicketForm() {
           <option value="started">Started</option>
           <option value="done">Done</option>
         </select>
-        <input type="submit" className='btn' value="Create Ticket" />
+        <input type="submit" className="btn" value={EDITMODE ? 'Update Ticket' : 'Create Ticket'} />
       </form>
     </div>
   );
--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -1,14 +1,25 @@
-import Ticket from "@/app/(models)/Ticket";
-import { NextResponse } from "next/server";
+import Ticket from '@/app/(models)/Ticket';
+import { NextResponse } from 'next/server';
 
-export async function DELETE(req, {params}) {
+export async function GET(req, { params }) {
   try {
-    
-    const {id} = params;
-    await  Ticket.findByIdAndDelete(id);
+    const { id } = params;
 
-    return NextResponse.json({message: "Ticket Deleted"}, { status: 200 });
+    const foundTicket = await Ticket.findOne({ _id: id });
+
+    return NextResponse.json({ foundTicket }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({message: "Error", error}, { status: 500 });
+    return NextResponse.json({ message: 'Error', error }, { status: 500 });
+  }
+}
+
+export async function DELETE(req, { params }) {
+  try {
+    const { id } = params;
+    await Ticket.findByIdAndDelete(id);
+
+    return NextResponse.json({ message: 'Ticket Deleted' }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: 'Error', error }, { status: 500 });
   }
 }
