bb16d653ae94a40ec317237a36a645733e758a89
Ryan Carr <ryan.g.carr@hotmail.com>
2023-11-10 21:06:15 -0500

    add status display and progress display

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,9 +1,15 @@
 import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+import StatusDisplay from "./StatusDisplay";
 
 export default function TicketCard() {
   return (
     <div>
       <DeleteBlock />
+      <PriorityDisplay />
+      <ProgressDisplay />
+      <StatusDisplay />
     </div>
   )
 }
